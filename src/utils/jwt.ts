a262202de5645620b30ac97da1e69911d2d4f388
tarekul42 <tarekulrifat205@gmail.com>
2025-10-28 02:01:52 +0600

    refactor(utils/jwt): added custom TokenError for better error handling

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,9 +1,16 @@
 import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
 
+class TokenError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = "TokenError";
+  }
+}
+
 const generateToken = (
   payload: JwtPayload,
   secret: string,
-  expiresIn: string,
+  expiresIn: string
 ) => {
   const token = jwt.sign(payload, secret, {
     expiresIn,
@@ -13,10 +20,10 @@ const generateToken = (
 
 const verifyToken = (token: string, secret: string) => {
   try {
-    const decodedToken = jwt.verify(token, secret);
+    const decodedToken = jwt.verify(token, secret) as JwtPayload;
     return decodedToken;
   } catch (error) {
-    throw new Error(`Invalid token ${error}`);
+    throw new TokenError(`Invalid token: ${(error as Error).message}`);
   }
 };
 
